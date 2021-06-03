90d1c2c5b4d7587e24473469c9d5927582900a40
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2021-06-03 09:56:34 +0100

    “Project”

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }, { path: 'feature', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) }, { path: 'public', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
